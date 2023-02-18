6b6b9be0eff1999409437666d68217837983dba4
DeshAn Hettiarachchi <deshan.h@parallax.lk>
2023-02-18 14:16:11 +0530

    functions developped

---
+++ app/Models/Data.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Data extends Model
+{
+    use HasFactory;
+    protected $table = 'data';
+    protected $primaryKey = 'id';
+    protected $fillable = ['actual_name','short_name','allow_decimal'];
+}
---
+++ resources/views/Datas/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('datas.layout')
+@section('content')
+    <div class="container">
+        <div class="row" style="margin:20px">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">Edit Unit</div>
+                    <div class="card-body">
+                        <form action="{{ url('datas/'.$datas -> id) }}" method="post">
+                        {!! csrf_field() !!}
+                        @method("PATCH")
+                        <input type="text" name="actual_name" id="actual_name" value="{{ $datas -> actual_name }}" class="form-control"></br>
+                        <input type="text" name="short_name" id="short_name" value="{{ $datas -> short_name }}" class="form-control" placeholder="Enter Short Name"></br>
+                        <input type="text" name="allow_decimal" id="allow_decimal" value="{{ $datas -> allow_decimal }}" class="form-control" placeholder="Enter Allow Decimal"></br>
+                        <input type="submit" value="Update" class="btn btn-success">
+                        
+                    </form>
+                    </div>
+                    </div>
+
+                    @stop
